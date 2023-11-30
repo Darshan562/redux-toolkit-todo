99695cc0dbc6b29379ff44e195374c2e898e0452
Darshan Vora <darshanvora562@gmail.com>
2023-11-30 21:14:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Redux toolkit TODO App.</h1>
+        <AddTodo />
+        <Todos />
+      </div>
+    </>
+  )
+}
+
+export default App
